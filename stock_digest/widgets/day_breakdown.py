9a82bcef2995899636ead83c116ae8fd8d2cdfe3
lucashadfield <lucas.hadfield@gmail.com>
2020-11-22 08:49:37 +1100

    adding stock prices to daily breakdown

--- stock_digest/widgets/day_breakdown.py
+++ stock_digest/widgets/day_breakdown.py
@@ -5,25 +5,46 @@ from stock_digest import Widget
 
 class DayBreakdownWidget(Widget):
     def plot(self, ax: plt.axes) -> plt.axes:
-        plot_df = self.portfolio.df.daily_change.iloc[-1].sort_values(ascending=False)
+        plot_df = (
+            self.portfolio.df.daily_change.iloc[-1]
+            .sort_values(ascending=False)
+            .rename('daily_change')
+            .to_frame()
+            .join(self.portfolio.df.prices.iloc[-1].rename('prices').to_frame())
+        )
 
-        plot_df.plot(ax=ax, kind='bar', width=0.95, lw=0, ec=self.BASE)
+        plot_df.daily_change.plot(ax=ax, kind='bar', width=0.95, lw=0, ec=self.BASE)
 
-        for p, symbol in zip(ax.patches, plot_df.keys()):
+        for p, symbol, prices in zip(ax.patches, plot_df.index, plot_df.prices):
             pos = p.get_height() >= 0
 
             p.set_facecolor(self._colour(p.get_height()))
+            # ticker symbols
             ax.annotate(
                 self._symbol_fix(symbol),
                 (p.get_x() + p.get_width() / 2, 0),
                 ha='center',
                 va='top' if pos else 'bottom',
                 color=self.BASE,
-                size=12,
+                size=10,
                 weight='bold',
-                xytext=(0, -2 if pos else 2),
+                xytext=(0, -2 if pos else 9),
                 textcoords='offset pixels',
             )
+
+            # stock prices
+            ax.annotate(
+                f'{prices:.2f}' if prices > 1 else f'{prices:.3f}',
+                (p.get_x() + p.get_width() / 2, 0),
+                ha='center',
+                va='top' if pos else 'bottom',
+                color=self.BASE,
+                size=8,
+                xytext=(0, -15 if pos else 0),
+                textcoords='offset pixels',
+            )
+
+            # daily change
             ax.annotate(
                 self._dollars(p.get_height()),
                 (p.get_x() + p.get_width() / 2, p.get_height()),
@@ -31,8 +52,8 @@ class DayBreakdownWidget(Widget):
                 va='bottom' if pos else 'top',
                 color=self.BASE,
                 weight='bold',
-                size=12,
-                xytext=(0, 2 if pos else -2),
+                size=10,
+                xytext=(0, 1 if pos else -2),
                 textcoords='offset pixels',
             )
 
